1c2fa7f50bf299dc589b812c15d97fe7205327d9
Eldad Chidra <eldadchb@gmail.com>
2023-07-02 22:13:20 +0200

    added: HomePage 1st structure

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+function HomePage() {
+  return (
+    <>
+      <div className="tool-bar">
+        <h1>Activity Scheduler</h1>
+      </div>
+      <div className="activities-container">list</div>
+    </>
+  );
+}
+
+export default HomePage;
