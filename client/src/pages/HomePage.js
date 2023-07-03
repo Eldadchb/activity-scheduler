40304e9a1566d343affb121c3932dbb6c43cd1e2
Eldad Chidra <eldadchb@gmail.com>
2023-07-03 15:19:57 +0200

    added: minimal and responsive style

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,19 +1,18 @@
+import "./HomePage.css";
 import Dashboard from "../components/dashboard/Dashboard";
 import Weather from "../components/weather/Weather";
 
 function HomePage() {
-  console.log("RERENDERED!!!!");
-
   return (
-    <>
+    <div className="home-page">
       <div className="tool-bar">
         <h1>Activity Scheduler</h1>
-        <Weather /> 
+        <Weather />
       </div>
       <div className="activities-container">
         <Dashboard />
       </div>
-    </>
+    </div>
   );
 }
 
