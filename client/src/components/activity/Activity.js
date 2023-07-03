4b0ae58f794739afe0f933cd43764c9c4c5b7b5d
Eldad Chidra <eldadchb@gmail.com>
2023-07-03 12:15:38 +0200

    fixed: Informative var names

--- client/src/components/activity/Activity.js
+++ client/src/components/activity/Activity.js
@@ -1,7 +1,7 @@
 import "./Activity.css";
 import { dateFormatter } from "../../utils/dateFormater";
 
-function Activity({ activity, date, performer, pitch, onEdit, onRemove }) {
+function Activity({ activityName, date, performer, pitch, onEdit, onRemove }) {
   // Prittier format for date - CAN BE MODIFIED
   const formatedDate = dateFormatter(date);
 
@@ -9,14 +9,14 @@ function Activity({ activity, date, performer, pitch, onEdit, onRemove }) {
     <div className="activity-box">
       <div className="date-left-side">{formatedDate.formatedDateShort}</div>
       <div className="activity-info">
-        <div className="activity-name">{activity}</div>
+        <div className="activity-name">{activityName}</div>
         <div className="activity-date">{formatedDate.formatedDateLong}</div>
         <div className="activity-performer">{performer}</div>
         <div className="activity-pitch">{pitch}</div>
       </div>
       <div className="change-button">
-        <input type="button" value="Edit" onClick={() => onEdit(activity.id)} />
-        <input type="button" value="Remove" onClick={() => onRemove(activity.pitch, activity.id)}/>
+        <input type="button" value="Edit" onClick={() => onEdit()} />
+        <input type="button" value="Remove" onClick={() => onRemove()}/>
       </div>
     </div>
   );
