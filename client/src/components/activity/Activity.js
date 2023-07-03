b0bab5f5dd31da83b3745882b1f1e8de9152490e
Eldad Chidra <eldadchb@gmail.com>
2023-07-03 11:11:54 +0200

    removed: date formatter - moved to dateFormatter utils

--- client/src/components/activity/Activity.js
+++ client/src/components/activity/Activity.js
@@ -4,15 +4,12 @@ import { dateFormatter } from "../../utils/dateFormater";
 function Activity({ activity, date, performer, pitch, onEdit, onRemove }) {
   const formatedDate = dateFormatter(date);
 
-  const formatedDateShort = `${formatedDate.formatedDay} ${formatedDate.formatedMonthShort}`;
-  const formatedDateLong = `${formatedDate.formatedTime} - ${formatedDate.formatedMonthLong} ${formatedDate.formatedDay}, ${formatedDate.formatedYear}`;
-
   return (
     <div className="activity-box">
-      <div className="date-left-side">{formatedDateShort}</div>
+      <div className="date-left-side">{formatedDate.formatedDateShort}</div>
       <div className="activity-info">
         <div className="activity-name">{activity}</div>
-        <div className="activity-date">{formatedDateLong}</div>
+        <div className="activity-date">{formatedDate.formatedDateLong}</div>
         <div className="activity-performer">{performer}</div>
         <div className="activity-pitch">{pitch}</div>
       </div>
