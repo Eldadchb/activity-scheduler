66b99811bcd43ac24567ce371c436fca3968ad18
Eldad Chidra <eldadchb@gmail.com>
2023-07-03 12:23:32 +0200

    removed: console.logs

--- client/src/services/weatherApiService.js
+++ client/src/services/weatherApiService.js
@@ -6,7 +6,6 @@ export async function getWeather(location) {
 
   try {
     const response = await axios.get(url);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(`Error: ${error}`);
