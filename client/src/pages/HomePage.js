b64e1134203f92611eaa2c94b7533dbc45273e40
Eldad Chidra <eldadchb@gmail.com>
2023-07-03 08:52:25 +0200

    added: Weather component

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,18 @@
+import Dashboard from "../components/dashboard/Dashboard";
+import Weather from "../components/weather/Weather";
+
 function HomePage() {
+  console.log("RERENDERED!!!!");
+
   return (
     <>
       <div className="tool-bar">
         <h1>Activity Scheduler</h1>
+        <Weather /> 
       </div>
-      <div className="activities-container">list</div>
+      <div className="activities-container">
+        <Dashboard />
+      </div>
     </>
   );
 }
